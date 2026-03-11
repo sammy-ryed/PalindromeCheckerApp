5a1206ba7e0302fd63695b40f20f99f947c0ded2
sammy_ryed <edwardsamarth@gmail.com>
2026-03-11 22:14:58 +0530

    Base application setup

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,10 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("========================================");
+        System.out.println("   Welcome to Palindrome Checker App   ");
+        System.out.println("========================================");
+        System.out.println("Application Name : Palindrome Checker App");
+        System.out.println("Version          : 1.0");
+        System.out.println("========================================");
+    }
+}
